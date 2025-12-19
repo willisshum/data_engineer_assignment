b392d784acfa2d4eeffb2612a28509bec675bde8
Willis Shum <willisshum@gmail.com>
2025-12-19 10:00:58 +0800

    Add logging

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,35 @@
 from dotenv import load_dotenv
+import os
+import logging
 
 load_dotenv()
+DICT_LOG_LEVEL_REFERENCE = {
+    "CRITICAL": logging.CRITICAL,
+    "ERROR": logging.ERROR,
+    "WARNING": logging.WARNING,
+    "INFO": logging.INFO,
+    "DEBUG": logging.DEBUG
+}
+LOG_LEVEL = DICT_LOG_LEVEL_REFERENCE[os.environ.get("LOG_LEVEL", "INFO")]
+
+logging.basicConfig(
+    format="[%(asctime)s][%(name)-5s][%(levelname)-5s] %(message)s (%(filename)s:%(lineno)d)",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=LOG_LEVEL
+)
 
 if __name__ == "__main__":
     # Ingest CSV data
+    logging.info('Ingest CSV data.')
     # Cleanse data
+    logging.info('Cleanse data.')
     # Deduplicate records
+    logging.info('Deduplicate records.')
     # Validate data against business rules
+    logging.info('Validate against business rules.')
     # Transform fields to fit MySQL schema
+    logging.info('Transform to fit MySQL schema.')
     # Load clean data into MySQL tables
+    logging.info('Load to MySQL tables.')
     # Quarantine rejected/problematic records for manual review
+    logging.info('Quarantine rejected/problematic records.')
