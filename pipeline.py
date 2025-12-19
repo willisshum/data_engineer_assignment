e81815c99a2f1e67865b8a5b2b23565548a6a91e
Willis Shum <willisshum@gmail.com>
2025-12-19 09:51:03 +0800

    Add pipeline.py with task titles

---
+++ pipeline.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    # Ingest CSV data
+    # Cleanse data
+    # Deduplicate records
+    # Validate data against business rules
+    # Transform fields to fit MySQL schema
+    # Load clean data into MySQL tables
+    # Quarantine rejected/problematic records for manual review
